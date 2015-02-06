586648bf8191c2defa020c8c4354156f4eecfc27
Zalila <ay.zalila@gmail.com>
2015-02-06 15:39:53 -0500

    10

--- app/src/main/java/com/memory/memory/model/Grid.java
+++ app/src/main/java/com/memory/memory/model/Grid.java
@@ -15,17 +15,46 @@ public class Grid {
     public Grid(int column, int row) {
         Vector<Card> cards = selectCards((column*row)/2);
 
+        //TODO Random
 
+        Vector<Card> rows;
+        for (int i = 0; i < row ; i++){
+            rows = new Vector<>();
+            for (int j = 0; j < column ; j++){
+                if (((i*column)+ j) < cards.size())
+                    rows.add(cards.get(i*column+j));
+                else
+                    rows.add(cards.get((i*column) + j - cards.size()));//((i+1)*(j+1))-cards.size()));
+            }
+            cardGrid.add(rows);
+        }
+        /*int i = 0, j = 0 ;rows = new Vector<>();
+        while(cards.iterator().hasNext()){
+            if ((j==0)&&(i!=0))    {cardGrid.add(rows);rows = new Vector<>();}
+            if (j==4)    {j=0;i++;}
+            rows.add(cards.iterator().next());
+            j++;
+        }*/
     }
 
     private Vector<Card> selectCards(int number) {
         Vector<Card> cards = new Vector<>();
-        for (int i = 0; i < number; i++)
+        Card c;
+        for (int i = 0; i < number ; i++)
         {
-            Card c = new Card();
+            c = new Card();
             c.setId(images[i]);
+            cards.add(c);
         }
         return cards;
+    }
+
+    public Vector<Vector<Card>> getCardGrid() {
+        return cardGrid;
+    }
+
+    public void setCardGrid(Vector<Vector<Card>> cardGrid) {
+        this.cardGrid = cardGrid;
     }
 
     // references to our images
