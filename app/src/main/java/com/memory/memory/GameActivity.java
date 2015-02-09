2e662bc9e12d7944fcded1455685528c3079260b
Zalila <ay.zalila@gmail.com>
2015-02-09 14:56:08 -0500

    who's first ?

--- app/src/main/java/com/memory/memory/GameActivity.java
+++ app/src/main/java/com/memory/memory/GameActivity.java
@@ -31,23 +31,31 @@ public class GameActivity extends ActionBarActivity {
         final GridView gridview = (GridView) findViewById(R.id.gameGrid);
         gridview.setAdapter(new ImageAdapter(this));
 
-        final Grid grid = new Grid(4,6);
+        final Grid grid = new Grid(4, 6);
 
         playerOne = getIntent().getExtras().getParcelable("playerOne");
-        TextView playerOneView = (TextView)findViewById(R.id.playerOneTextView);
+        TextView playerOneView = (TextView) findViewById(R.id.playerOneTextView);
         playerOneView.setText(playerOne.getName());
         playerOneView.setRotation(90);
 
         playerTwo = getIntent().getExtras().getParcelable("playerTwo");
-        TextView playerTwoView = (TextView)findViewById(R.id.playerTwoTextView);
+        TextView playerTwoView = (TextView) findViewById(R.id.playerTwoTextView);
         playerTwoView.setText(playerTwo.getName());
         playerTwoView.setRotation(270);
+
+        if (Math.random() < 0.5) {
+            Toast.makeText(GameActivity.this, playerOne.getName() + " commence la partie", Toast.LENGTH_SHORT).show();
+            ((ImageView) findViewById(R.id.playerOneToken)).setImageResource(R.drawable.circle_green);
+        } else{
+            Toast.makeText(GameActivity.this, playerTwo.getName() + " commence la partie", Toast.LENGTH_SHORT).show();
+            ((ImageView) findViewById(R.id.playerTwoToken)).setImageResource(R.drawable.circle_green);
+        }
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 ImageView iv = (ImageView) gridview.getAdapter().getView(position, v, parent);
-                int column = position % 4;
-                int row = position / 4;
+                //int column = position % 4;
+                //int row = position / 4;
                 iv.setImageResource(grid.getCardGrid().get(position).getId());
 //                Toast.makeText(GameActivity.this, "" + position, Toast.LENGTH_SHORT).show();
             }
